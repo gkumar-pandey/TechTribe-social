233d229a331fd435349e64480d0ed635e9b19717
gkumar-pandey <gautamshekhar078@gmail.com>
2023-08-31 06:09:55 +0530

    style:make bg blur

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -9,12 +9,12 @@ const Modal = ({ children, onClose, isOpen, title }) => {
 
   return createPortal(
     <>
-      <div className="fixed inset-0 flex items-center justify-center z-50 ">
+      <div className="fixed inset-0  backdrop-blur-[2px] flex items-center justify-center z-50 ">
         <div
-          className="fixed inset-0 bg-black opacity-30"
+          className="fixed inset-0 bg-black opacity-10"
           onClick={onClose}
         ></div>
-        <div className="bg-white dark:bg-[color:var(--secondary-dark-color)] dark:text-[color:var(--text-color)] rounded-lg p-4 z-10 max-w-[40rem] w-full mx-1">
+        <div className="bg-white shadow-lg  dark:bg-[color:var(--secondary-dark-color)] dark:text-[color:var(--text-color)] rounded-lg p-4 z-10 max-w-[40rem] w-full mx-1">
           <div className=" flex justify-between items-center ">
             <h2 className="text-lg font-extrabold ">{title}</h2>
             <p className=" hover:bg-slate-100 rounded-full p-2 dark:hover:bg-[color:var(--dark-color)] ">
